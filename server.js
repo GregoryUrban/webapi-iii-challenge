c53ba68643f5901a68661fe414eee020c80077c3
GregoryUrban <turknben@gmail.com>
2019-05-14 23:15:01 -0400

    Logger get function

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = 'express';
+const express = require('express'); // importing a CommonJS module
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json()); 
+
+
+server.get('/', (req, res, next) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
+  next();
 });
 
 //custom middleware
+// const logger = require('./middleware/logger.js'); // importing a CommonJS module
 
 function logger(req, res, next) {
-
+  console.log(`${new Date().toISOString()}: ${req.method} to ${req.url}`)
+  next()
 };
 
 module.exports = server;
